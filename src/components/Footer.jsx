460aca088c5a2226fade5405a899dcb8cdadb914
Axel LAPLACE <axellaplace1709@gmail.com>
2023-09-15 18:03:32 +0200

    Integration du Header et Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
+import '../styles/footer.scss'
+
+import img from '../assets/kasa-logo-white.svg'
+
 function Footer() {
     return (
       <div className="footer">
-       
+        <img src={img} alt='logo kasa blanc'/>
+        <span>© Kasa. All rights reserved</span>
       </div>
     );
   }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 import { NavLink } from "react-router-dom";
+import img from '../assets/kasa-logo.svg'
+import '../styles/header.scss';
 
 function Header() {
     return (
       <div className="header">
+        <img src={img} alt="logo kasa"/>
         <nav>
             <ul>
                 <li>
                     <NavLink to="/" className={({ isActive }) => (isActive ? "activeLink" : undefined )}>Acceuil</NavLink>
                 </li>
-
                 <li>
                     <NavLink to="/about" className={({ isActive }) => (isActive ? "activeLink" : undefined )}>À propos</NavLink>
                 </li>
             </ul>
         </nav>
-       
       </div>
     );
   }
