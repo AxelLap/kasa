e3c1c2f96911c42ea82229ac2a952497b83c3036
Axel LAPLACE <axellaplace1709@gmail.com>
2023-09-19 14:59:53 +0200

    error page added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+import img from '../assets/not-found.svg'
+
+import '../styles/pages/error.scss'
 
 function Error() {
   return (
     <div className="error">
-
+      <img src={img} alt="logo erreur 404" />
+      <span>Oups ! La page que vous demandez n'éxiste pas</span>
+      <NavLink to="/" className="returnHome">Retourner sur la page d'acceuil</NavLink>
     </div>
   );
 }
