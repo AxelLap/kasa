c0f32a7aa8ac10081b1a31c99f0f50c53f43f4e9
Axel LAPLACE <axellaplace1709@gmail.com>
2023-09-22 14:50:28 +0200

    annimation for collapse components added

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,33 +1,40 @@
+//hooks
 import { useState } from 'react';
-import arrow from '../assets/arrow-down.svg'
-import '../styles/components/collapse.scss'
+//images
+import arrow from '../assets/arrow-inactive.svg'
+//style
+//import '../styles/components/collapse.scss'
 
 function Collapse(props) {
     const [open, setOPen] = useState(false);
     const { title, description } = props;
 
+
+
     const handleCollapsing = () => {
         setOPen(!open)
     }
+
 
     return (
         <div className='collapse-wrapper'>
             <div className='collapse-header'>
                 <h2>{title}</h2>
-                <button onClick={handleCollapsing}>
-                    <img src={arrow} alt='flèche ouverture' className='arrow' />
+                <button onClick={handleCollapsing} >
+                    <div className={open ? "arrow-active" : "arrow-inactive"}>
+                        <img src={arrow} alt='flèche ouverture' className='arrow' />
+                    </div>
                 </button>
-
             </div>
-            {
-                open &&
-                //<div className={open ? "content-show" : "content-hide"}>
+
+
+            <div className={open ? "content-show" : "content-hide"}>
                 <div className='collapse-content'>
                     <p>{description}</p>
                 </div>
-                //</div>
+            </div>
 
-            }
+
         </div>
     )
 }
