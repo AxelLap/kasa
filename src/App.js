8f00565e4a05e7ef60a7b705862989229aa54b5d
Axel LAPLACE <axellaplace1709@gmail.com>
2023-12-15 16:37:15 +0100

    mise en page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './styles/css/pages.css'
 import './styles/css/components.css'
@@ -12,17 +12,17 @@ import About from './pages/about/About';
 import Error from './pages/error/Error';
 import Accomodation from './pages/accomodation/Accomodation'
 
-function App(){
-    return(
+function App() {
+    return (
         <BrowserRouter>
-        <Header/>
-        <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/about' element={<About/>}/>
-            <Route path='accomodation/:id' element={<Accomodation/>}/>
-            <Route path='/*' element={<Error/>}/>
-        </Routes>
-        <Footer/>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/about' element={<About />} />
+                <Route path='accomodation/:id' element={<Accomodation />} />
+                <Route path='/*' element={<Error />} />
+            </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
